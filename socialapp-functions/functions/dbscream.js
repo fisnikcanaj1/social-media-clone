d0cb3727e78d496befe0729266af97c55e5355b6
Fisnik <fisnik-canaj-test@gmail.com>
2020-10-10 11:38:51 +0200

    added firebase login with token

---
+++ socialapp-functions/functions/dbscream.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream',
+            created: '2020-10-09T15:16:16.568Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
